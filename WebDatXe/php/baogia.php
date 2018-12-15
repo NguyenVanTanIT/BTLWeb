17fd57f18a8e3afa52d68c3b5a20a6975a2362e2
trangnguyen0924 <trangthuy0924@gmail.com>
2018-12-16 00:40:28 +0700

    them bao gia

---
+++ WebDatXe/php/baogia.php
@@ -0,0 +1,84 @@
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+		<h2 id="gt_tieude" style="color: red;"><span style="font-size: x-large;">CÔNG TY TNHH VẬN TẢI LỮ HÀNH TÂN TRANG</span></h2>
+		<p class="gt_diachi"><strong>Địa chỉ :&nbsp;Địa chỉ: 175 Tây Sơn Đống Đa Hà Nội</strong></p>
+		 <p class="gt_lienhe"><strong>Hotline :&nbsp;<span style="color: #ff0000; font-size: large;">0398712777 &nbsp;- &nbsp;0943074999</span></strong></p>
+			<div class="gt_image">
+				<img src="image/baogia.jpg" alt="Los Angeles" width="825" height="250">
+			</div>
+		<div class="col-md-12">
+			<p class="gt_congty">  Dịch vụ đặt xe Tân Trang cung cấp bảng giá để các bạn tham khảo, tránh những gian lận về giá</p>
+			<p class="gt_congty">Kính Chúc quý khách hàng luôn có được sức khỏe, hạnh phúc, thành công và có những chuyến đi vui vẻ bên gia đình và người thân!</p>
+	<h2 id="gt_tieude" style="color: red;"><span style="font-size: x-large;">BẢNG GIÁ THUÊ XE TỪ 4 CHỖ ĐẾN 45 CHỖ</span></h2>
+
+
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-2">Yêu cầu
+		</div>
+		<div class="col-md-2">Xe 4 Chỗ
+		</div>
+		<div class="col-md-2">Xe 8 Chỗ
+		</div>
+		<div class="col-md-2">Xe 16 Chỗ
+		</div>
+		<div class="col-md-2">Xe 35 Chỗ
+		</div>
+		<div class="col-md-2">Xe 45 Chỗ
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-2">Có Tài Xế
+		</div>
+		<div class="col-md-2">1.100.000
+		</div>
+		<div class="col-md-2">1.200.000
+		</div>
+		<div class="col-md-2">1.400.000
+		</div>
+		<div class="col-md-2">1.600.000
+		</div>
+		<div class="col-md-2">1.900.000
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-2">Tự Lái
+		</div>
+		<div class="col-md-2">900.000
+		</div>
+		<div class="col-md-2">1.000.000
+		</div>
+		<div class="col-md-2">1.200.000
+		</div>
+		<div class="col-md-2">1.400.000
+		</div>
+		<div class="col-md-2">1.700.000
+		</div>
+	</div>
+</div>
+
+
+
+<p></p>
+<p></p>
+			<h4> Ghi Chú:</h4>
+			<p class="gt_congty">
+				<p>- Đơn giá tính theo đơn vị ngày</p>
+				<p>– Lái xe chuyên nghiệp, ăn mặc lịch sự, nhiều kinh nghiệm.</p>
+				<p>– Số lượng các loại xe cung cấp đủ, số lượng xe lớn, các chủng loại xe, đáp ứng mọi nhu cầu của khách.</p>
+				<p>– Giá trên đã bao gồm xăng xe, cầu phà, lương lái xe. Không bao gồm vé cao tốc</p>
+				<p>– Giá trên chưa bao gồm 10% thuế GTGT.</p>
+				<p>– Báo giá trên có thể thay đổi tại thời điểm đặt xe, chủng loại xe</p>
+				<p>Qúy khách vui lòng liên hệ để được tư vấn và được giá tốt nhất.</p>
+			</p>
+			<p class="gt_congty">Hãy đến với chúng tôi,Tân Trang cam kết quý khách sẽ có những trải nghiệm dịch vụ thật sự trân thực, gần gũi và chuyên nghiệp. chắc chắn bạn sẽ có sự hài lòng tuyệt đối và luôn có nụ cười vui vẻ sau mỗi chuyến đi!</p>
+			<h3 class="gt_ketthuc">Cảm ơn quý khách đã tin tưởng và dùng dịch vụ của chúng tôi</h3>
+			
+			
+
+		</div>
+		</div>
+	</div>
+</div>
