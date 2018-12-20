a9d0f4ac8ffe2dbeac7c228f9836b553703999ef
NguyenVanTanIT <nvtanit95@gmail.com>
2018-12-20 10:56:12 +0700

    no message

--- N034862/Sources/admin/car/updatecar.php
+++ N034862/Sources/admin/car/updatecar.php
@@ -11,10 +11,10 @@
         <div class="container">  
             <br />
    <div class="table-responsive">  
-    <h3 align="center">Update Multiple Mysql Data using Checkbox with Ajax in PHP</h3><br />
+    <h3 align="center">Update Xe</h3><br />
     <form method="post" id="update_form">
                     <div align="left">
-                        <input type="submit" name="multiple_update" id="multiple_update" class="btn btn-info" value="Multiple Update" />
+                        <input type="submit" name="multiple_update" id="multiple_update" class="btn btn-info" value=" Update xe" />
                     </div>
                     <br />
                     <div class="table-responsive">
@@ -42,7 +42,7 @@ $(document).ready(function(){
     function fetch_data()
     {
         $.ajax({
-            url:"car/select.php",
+            url:"select.php",
             method:"POST",
             dataType:"json",
             success:function(data)
@@ -51,60 +51,19 @@ $(document).ready(function(){
                 for(var count = 0; count < data.length; count++)
                 {
                     html += '<tr>';
-                    html += '<td><input type="checkbox" id="'+data[count].id+'" data-carname="'+data[count].carname+'" data-describe="'+data[count].describe+'" data-image="'+data[count].image+'" data-designation="'+data[count].designation+'" data-age="'+data[count].age+'" class="check_box"  /></td>';
+                    html += '<td><input type="checkbox" id_car="'+data[count].id_car+'" data-carname="'+data[count].carname+'" data-describe="'+data[count].describe+'" data-image="'+data[count].image+'" data-seats="'+data[count].seats+'" data-licenseplate="'+data[count].licenseplate+'" data-license="'+data[count].license+'" class="check_box"  /></td>';
+
                     html += '<td>'+data[count].carname+'</td>';
                     html += '<td>'+data[count].describe+'</td>';
                     html += '<td>'+data[count].image+'</td>';
-                    html += '<td>'+data[count].designation+'</td>';
-                    html += '<td>'+data[count].age+'</td></tr>';
+                    html += '<td>'+data[count].seats+'</td>';
+                    html += '<td>'+data[count].licenseplate+'</td></tr>';
+                    html += '<td>'+data[count].license+'</td></tr>';
                 }
                 $('tbody').html(html);
             }
         });
     }
-
-    fetch_data();
-
-    $(document).on('click', '.check_box', function(){
-        var html = '';
-        if(this.checked)
-        {
-            html = '<td><input type="checkbox" id="'+$(this).attr('id')+'" data-carname="'+$(this).data('carname')+'" data-describe="'+$(this).data('describe')+'" data-image="'+$(this).data('image')+'" data-designation="'+$(this).data('designation')+'" data-age="'+$(this).data('age')+'" class="check_box" checked /></td>';
-            html += '<td><input type="text" carname="carname[]" class="form-control" value="'+$(this).data("carname")+'" /></td>';
-            html += '<td><input type="text" carname="describe[]" class="form-control" value="'+$(this).data("describe")+'" /></td>';
-            html += '<td><select carname="image[]" id="gender_'+$(this).attr('id')+'" class="form-control"><option value="Male">Male</option><option value="Female">Female</option></select></td>';
-            html += '<td><input type="text" carname="designation[]" class="form-control" value="'+$(this).data("designation")+'" /></td>';
-            html += '<td><input type="text" carname="age[]" class="form-control" value="'+$(this).data("age")+'" /><input type="hidden" carname="hidden_id[]" value="'+$(this).attr('id')+'" /></td>';
-        }
-        else
-        {
-            html = '<td><input type="checkbox" id="'+$(this).attr('id')+'" data-carname="'+$(this).data('carname')+'" data-describe="'+$(this).data('describe')+'" data-image="'+$(this).data('image')+'" data-designation="'+$(this).data('designation')+'" data-age="'+$(this).data('age')+'" class="check_box" /></td>';
-            html += '<td>'+$(this).data('carname')+'</td>';
-            html += '<td>'+$(this).data('describe')+'</td>';
-            html += '<td>'+$(this).data('image')+'</td>';
-            html += '<td>'+$(this).data('designation')+'</td>';
-            html += '<td>'+$(this).data('age')+'</td>';            
-        }
-        $(this).closest('tr').html(html);
-        $('#gender_'+$(this).attr('id')+'').val($(this).data('image'));
-    });
-
-    $('#update_form').on('submit', function(event){
-        event.preventDefault();
-        if($('.check_box:checked').length > 0)
-        {
-            $.ajax({
-                url:"multiple_update.php",
-                method:"POST",
-                data:$(this).serialize(),
-                success:function()
-                {
-                    alert('Data Updated');
-                    fetch_data();
-                }
-            })
-        }
-    });
-
+fetch_data();
 });  
 </script>
